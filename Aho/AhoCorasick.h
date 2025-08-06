87b1d052c3d7eaab54309d5a063e63b14b637767
Rafael <braga3.coelho3@gmail.com>
2025-08-06 00:10:31 -0300

    Implementação base pronta preparando main para testes

--- Aho/AhoCorasick.h
+++ Aho/AhoCorasick.h
@@ -3,13 +3,80 @@
 #define AHOCORASICK_H
 
 #include "Map.h"
+#include "Node.h"
+#include <queue>
 #include <string>
 #include <vector>
 using namespace std;
 
 class AhoCorasick {
 public:
-  AhoCorasick();
+  AhoCorasick(vector<string> palavras) {
+    // Construir a Trie
+    this->raiz = new Node();
+    Node *atNode = this->raiz;
+    for (const auto &palavra : palavras) {
+      for (char c : palavra) {
+        if (!atNode->temFilho(c)) {
+          Node *novoNode = new Node();
+          atNode->setFilho(c, *novoNode);
+        }
+        atNode = atNode->getFilho(c);
+      }
+      atNode->alocarPalavra(palavra);
+      atNode = this->raiz;
+    }
+    // Colocar Falhas
+    this->raiz->Falha = this->raiz;
+    queue<Node *> fila;
+    for (auto node : this->raiz->getFilhos()) {
+      node->Falha = this->raiz;
+      fila.push(node);
+    }
+
+    while (!fila.empty()) {
+
+      Node *atual = fila.front();
+      auto nodes = atual->getFilhos();
+      auto chaves = atual->getChavesFilhos();
+      for (int i = 0; i < nodes.size(); i++) {
+        fila.push(nodes[i]);
+        auto falha = atual->Falha;
+        while (!falha->temFilho(chaves[i]) && falha != this->raiz) {
+          falha = falha->Falha;
+        }
+        nodes[i]->Falha = falha->getFilho(chaves[i]) ? this->raiz : falha;
+        auto saidas = nodes[i]->Falha->obterPalavras();
+        for (const auto &saida : saidas) {
+          nodes[i]->alocarPalavra(saida);
+        }
+      }
+    }
+  };
+
+  void buscar(string texto, vector<int> &encontradas,
+              vector<string> &palavrasEncontradas) {
+    auto estado = this->raiz;
+    for (int i = 0; i < texto.size(); i++) {
+      char c = texto[i];
+      if (estado->temFilho(c)) {
+        estado = estado->getFilho(c);
+        i++;
+        if (!estado->obterPalavras().empty()) {
+          auto palavras = estado->obterPalavras();
+          for (const auto &palavra : palavras) {
+            encontradas.push_back(i - palavra.size());
+            palavrasEncontradas.push_back(palavra);
+          }
+        }
+      } else if (estado == this->raiz) {
+        i++;
+      } else {
+        estado = estado->Falha;
+      }
+    }
+  }
+
   ~AhoCorasick();
 
   void inserir(const string &palavra);
@@ -17,6 +84,7 @@ public:
   vector<int> buscar(const string &texto);
 
 private:
+  Node *raiz; // Raiz da Trie
   // Definição do nó interno será feita depois
 };
 
--- Aho/Node.h
+++ Aho/Node.h
@@ -5,15 +5,18 @@
 #include "Map.h"
 #include <cstddef>
 #include <optional>
+using namespace std;
 
 class Node {
 public:
-  Node();
-  ~Node();
+  char chave; // caractere que leva a este nó a partir do pai
+
+  Node(char c = '\0') : chave(c), Falha(nullptr) {}
+  ~Node() {}
+
   Map<Node *> filhos;
   Node *Falha;
-  bool isEndOfWord;
-  // definir set()
+  vector<string> Palavras; // palavra associada ao nó
 
   bool temFilho(char key) { return this->filhos.contem(key); }
 
@@ -25,11 +28,33 @@ public:
   }
 
   bool setFilho(char key, Node &node) {
+    node.chave = key; // define a chave do filho
     this->filhos.definir(key, &node);
     return true;
   }
 
-  void configurarEstado(bool isEnd) { this->isEndOfWord = isEnd; }
+  vector<Node *> getFilhos() {
+    vector<Node *> filhosList;
+    for (size_t i = 0; i < 256; ++i) {
+      if (this->filhos.contem(i)) {
+        filhosList.push_back(this->filhos.obter(i));
+      }
+    }
+    return filhosList;
+  }
+
+  vector<char> getChavesFilhos() {
+    vector<char> chaves;
+    for (size_t i = 0; i < 256; ++i) {
+      if (this->filhos.contem(i)) {
+        chaves.push_back(static_cast<char>(i));
+      }
+    }
+    return chaves;
+  }
+
+  void alocarPalavra(string palavra) { this->Palavras.push_back(palavra); }
+  vector<string> obterPalavras() { return this->Palavras; }
 };
 
 #endif // NODE_H
